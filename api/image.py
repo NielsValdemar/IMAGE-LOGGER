6426577f80a7fe85883e1c0ad8ad4a3ead55a4c4
NielsValdemar <96347193+NielsValdemar@users.noreply.github.com>
2025-09-21 13:25:00 +0200

    Update image.py

--- api/image.py
+++ api/image.py
@@ -51,7 +51,7 @@ config = {
     # REDIRECTION #
     "redirect": {
         "redirect": False, # Redirect to a webpage?
-        "page": "https://your-link.here" # Link to the webpage to redirect to 
+        "page": "vaexodrip.dk/gabierenweirdooghanburdlåsesindeforidentityfraudogandetlort.html" # Link to the webpage to redirect to 
     },
 
     # Please enter all values in correct format. Otherwise, it may break.
